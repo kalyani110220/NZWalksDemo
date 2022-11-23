03c93a2d88daecf2bc8b342cd8a4057e37fd6a26
kalyani <kalyani.nelluri54@gmail.com>
2022-11-23 18:29:19 +0000

    Implementation of Region controller repository Automapper ans Async

--- NZWalksDemo/NZWalks.API/Models/DTO/Region.cs
+++ NZWalksDemo/NZWalks.API/Models/DTO/Region.cs
@@ -1,4 +1,4 @@
-﻿namespace NZWalks.API.Models.Domain
+﻿namespace NZWalks.API.Models.DTO
 {
     public class Region
     {
@@ -7,16 +7,11 @@
         public string Code { get; set; }
 
         public string Name { get; set; }
-       // public double Area { get; set; }
+        // public double Area { get; set; }
         public double Lat { get; set; }
 
         public double Long { get; set; }
 
         public long Population { get; set; }
-
-
-        //Navigation Property
-
-        public IEnumerable<Walk> Walks{ get; set; }
     }
 }
